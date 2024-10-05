e7e9af4b093da742a7b7a53869952b87085f67aa
Bourouail-Issam <issambourouail33@gmail.com>
2024-10-05 16:11:16 +0100

    Add functions members for generating random characters and keys in clsUtil class

--- My-All-Libarary/My-All-Libarary.cpp
+++ My-All-Libarary/My-All-Libarary.cpp
@@ -1,7 +1,17 @@
 
 #include <iostream>
+#include "clsUtil.h"
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+   clsUtil::Srand();
+   cout << clsUtil::RandomNumber(4, 17) << endl;
+   cout << clsUtil::GetRandomCharacter(clsUtil::MixChars) << endl;
+   cout << clsUtil::GeneratWord(clsUtil::SmallLetter, 8) << endl;
+   cout << clsUtil::GeneratKey(4) << endl;
+
+   cout << "Keys Generated : \n";
+   clsUtil::GenerateKeys(5);
 }
--- My-All-Libarary/clsUtil.h
+++ My-All-Libarary/clsUtil.h
@@ -1,5 +1,80 @@
 #pragma once
+#include <iostream>
+#include<cstdlib>
+using namespace std;
+
 class clsUtil
 {
+    public:
+
+       static enum EnCharType
+        {
+            SmallLetter = 1, CapitalLetter = 2, Digit = 3, MixChars = 4, SpecialCharacter = 5
+        };
+
+       static void  Srand()
+       {
+           //Seeds the random number generator in C++, called only once
+           srand((unsigned)time(NULL));
+       }
+
+       static int RandomNumber(int From, int To)
+       {
+           //Function to generate a random number
+
+           int randNum = rand() % (To - From + 1) + From;
+           return randNum;
+       }
+
+       static char GetRandomCharacter(EnCharType charType) 
+       {
+
+           if (charType == MixChars) 
+               charType = (EnCharType)RandomNumber(1, 3);
+           
+           switch (charType)
+           {
+           case clsUtil::SmallLetter:
+               return  char(RandomNumber(97, 122));
+
+           case EnCharType::CapitalLetter:
+               return  char(RandomNumber(65, 90));
+
+           case EnCharType::Digit:
+               return  char(RandomNumber(48, 57));
+
+           case EnCharType::SpecialCharacter:
+               return char(RandomNumber(33, 47));
+           }
+       }
+
+       static string GeneratWord(EnCharType TypeChar, short length)
+       {
+           string word = "";
+
+           for (int i = 0; i < length; i++)
+               word += GetRandomCharacter(TypeChar);
+           
+           return word;
+       }
+
+       static string GeneratKey(EnCharType TypeChar)
+       {
+           string KeyG = "";
+           for (int i = 0; i < 4; i++)
+           {
+               KeyG += GeneratWord(TypeChar, 4);
+               if (i <=  2)
+                   KeyG += '-';
+           }
+           return KeyG;
+       }
+
+       static void GenerateKeys(int NumberOfKeys, EnCharType TypeChar) 
+       {
+           for (int i = 1; i <= NumberOfKeys; i++)
+               cout << "Key [" << i << "] : " << GeneratKey(TypeChar) << endl;
+       }
+
 };
 
