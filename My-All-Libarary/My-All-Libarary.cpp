b5f842d5054c7c36c1f7961bd0c688f84a347c32
Bourouail-Issam <issambourouail33@gmail.com>
2024-10-06 18:13:26 +0100

    Add Swap functions for various data types and array manipulation functions in clsUtil class

--- My-All-Libarary/My-All-Libarary.cpp
+++ My-All-Libarary/My-All-Libarary.cpp
@@ -10,8 +10,48 @@ int main()
    cout << clsUtil::RandomNumber(4, 17) << endl;
    cout << clsUtil::GetRandomCharacter(clsUtil::MixChars) << endl;
    cout << clsUtil::GeneratWord(clsUtil::SmallLetter, 8) << endl;
-   cout << clsUtil::GeneratKey(4) << endl;
+   cout << clsUtil::GeneratKey(clsUtil::CapitalLetter) << endl;
 
    cout << "Keys Generated : \n";
-   clsUtil::GenerateKeys(5);
+   clsUtil::GenerateKeys(5,clsUtil::CapitalLetter);
+
+   int X1 = 1, X2 = 200;
+   clsUtil::Swap(X1, X2);
+   cout << "\nX1 = " << X1;
+   cout << "\nX2 = " << X2;
+
+   //string integer
+   int arr1[5] = { 1,2,3,4,5 };
+
+   clsUtil::ShuffleArray(arr1, 5);
+   cout << "\nArray After shuffle : \n";
+   for (int i = 0; i < 5; i++)
+   {
+	   cout << arr1[i] << endl;
+   }
+
+   //string array
+   string arr2[5] = { "Karim","issam","Ahmed","Mohammed","Anwer" };
+
+   clsUtil::ShuffleArray(arr2, 5);
+   cout << "\nArray After shuffle : \n";
+   for (int i = 0; i < 5; i++)
+   {
+	   cout << arr2[i] << endl;
+   }
+
+   int arr3[5];
+   clsUtil::FillArrayWithRandomNumber(arr3, 5,33,77);
+   cout << "\nArray After fill integer numbers : \n";
+   for (int i = 0; i < 5; i++)
+   {
+	   cout << arr3[i] << endl;
+   }
+
+   string arr4[5];
+   clsUtil::FillArrayWithRandomWords(arr4, 5, clsUtil::MixChars, 8);
+   for (int i = 0; i < 5; i++)
+   {
+      cout << arr4[i] << endl;
+   }
 }
--- My-All-Libarary/clsUtil.h
+++ My-All-Libarary/clsUtil.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include<cstdlib>
+#include"clsDate.h"
 using namespace std;
 
 class clsUtil
@@ -75,6 +76,82 @@ class clsUtil
            for (int i = 1; i <= NumberOfKeys; i++)
                cout << "Key [" << i << "] : " << GeneratKey(TypeChar) << endl;
        }
+       
+       //Swap integer
+       static void Swap(int& Num1, int& Num2) 
+       {
+           int TMP;
+           TMP = Num1;
+           Num1 = Num2;
+           Num2 = TMP;
+       }
+       
+       //Swap double
+       static void Swap(double& Num1, double& Num2) 
+       {
+           double TMP;
+           TMP = Num1;
+           Num1 = Num2;
+           Num2 = TMP;
+       }
 
-};
+       //Swap boolean
+       static void Swap(bool& booleanValue1, bool& booleanValue2)
+       {
+           bool TMP;
+           TMP = booleanValue1;
+           booleanValue1 = booleanValue2;
+           booleanValue2 = TMP;
+       }
+
+       //Swap Char
+       static void Swap(char& char1, char& char2)
+       {
+           char TMP;
+           TMP = char1;
+           char1 = char2;
+           char2 = TMP;
+       }
+
+       //Swap String
+       static void Swap(string& string1, string& string2)
+       {
+           string TMP;
+           TMP = string1;
+           string1 = string2;
+           string2 = TMP;
+       }
+
+       //Swap date
+       static void Swap(clsDate& Date1, clsDate& Date2)
+       {
+           clsDate TMP;
+           TMP = Date1;
+           Date1 = Date2;
+           Date2 = TMP;
+       }
+
+       static void ShuffleArray(int Arr[100], int ArrLength) {
+           for (int i = 0; i < ArrLength; i++)
+               Swap(Arr[RandomNumber(1, ArrLength) - 1], Arr[RandomNumber(1, ArrLength) - 1]);
+       }
+       static void ShuffleArray(string Arr[100], int ArrLength) {
+           for (int i = 0; i < ArrLength; i++)
+               Swap(Arr[RandomNumber(1, ArrLength) - 1], Arr[RandomNumber(1, ArrLength) - 1]);
+       }
+       static void FillArrayWithRandomNumber(int Arr[100], int Arraylength, int From, int To)
+       {
+           for (int i = 0; i < Arraylength; i++)
+           {
+               Arr[i] = RandomNumber(From, To);
+           }
+       }
+       static void FillArrayWithRandomWords(string Arr[100], int Arraylength,EnCharType CharType,int lengthOfWord)
+     {
+         for (int i = 0; i < Arraylength; i++)
+         {
+             Arr[i] = GeneratWord(CharType, lengthOfWord);
+         }
+     }
+}; 
 
